f3fa8cd953869a932a86810490e8df59cf893072
Rieethaa <rieethaa@gmail.com>
2012-02-02 20:47:20 +0000

    Ran 'rails generate model recipe title:string description:text' to create our recipe model and schema migration'

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
   def index
-    # @name is a member variable (indicated by the '@' prefix). Rails makes member variables of a controller
+    # @recipes is a member variable (indicated by the '@' prefix). Rails makes member variables of a controller
     # automatically accessible from the view template that renders the response for the action.
-    @name = 'Rieethaa'
+    @recipes = Recipe.all
   end
 end
