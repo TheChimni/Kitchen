f75e55864969624677631fbf3b4bf64dc35eca14
Rieethaa <rieethaa@gmail.com>
2012-02-04 18:13:15 +0000

    Added seed data for some sample recipes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Recipe.destroy_all
+
+Recipe.create!(:title => 'Baingan ka Bharta')
+Recipe.create!(:title => 'Shepu')
+Recipe.create!(:title => 'Dal Makhni')
+Recipe.create!(:title => 'Bhindi')
+
