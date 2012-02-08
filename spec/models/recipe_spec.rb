6d123da4842b3ddc00e0e4614713f81710dba5f4
Rieethaa <rieethaa@gmail.com>
2012-02-08 20:54:23 +0000

    Added some simple specs for the Recipe model

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,4 +1,22 @@
 require 'spec_helper'
 
 describe Recipe do
+  it 'should have a nil title by default' do
+    recipe = Recipe.new
+    recipe.title.should be_nil
+    # The previous line is equivalent to 
+    # recipe.title.nil?.should == true
+  end
+
+  it 'should allow me to set a title' do
+    recipe = Recipe.new(:title => 'Tasty recipe')
+    recipe.title.should_not be_nil
+    recipe.title.should == 'Tasty recipe'
+  end
+
+  it 'should not be valid without a title' do
+    recipe = Recipe.new
+    recipe.should_not be_valid
+  end
+
 end
