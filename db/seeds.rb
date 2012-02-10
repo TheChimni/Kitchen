71d72d660c8a31ac0cfa9e06cb9f38714ea49baa
Rieethaa <rieethaa@gmail.com>
2012-02-10 20:19:06 +0000

    Updated seeds file to handle new mandoatory attributes of Recipe

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
 Recipe.destroy_all
 
-Recipe.create!(:title => 'Baingan ka Bharta')
-Recipe.create!(:title => 'Shepu')
-Recipe.create!(:title => 'Dal Makhni')
-Recipe.create!(:title => 'Bhindi')
+Recipe.create!(:title => 'Baingan ka Bharta',
+               :description => 'Good old Punjabi food',
+               :ingredients => 'secret',
+               :preparation_method => 'secret')
+Recipe.create!(:title => 'Shepu',
+               :description => 'Marathi farmers food',
+               :ingredients => 'secret',
+               :preparation_method => 'secret')
+Recipe.create!(:title => 'Dal Makhni',
+               :description => 'Daadiji ki recipe',
+               :ingredients => 'secret',
+               :preparation_method => 'secret')
+Recipe.create!(:title => 'Bhindi',
+               :description => 'Eaten all over India',
+               :ingredients => 'secret',
+               :preparation_method => 'secret')
 
