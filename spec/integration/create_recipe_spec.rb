0a5a5edf1db405a227ea36683ea33b2dbeb74abd
Rieethaa <rieethaa@gmail.com>
2012-02-12 19:04:43 +0000

    Added a spec for error handling when creating a new recipe

--- spec/integration/create_recipe_spec.rb
+++ spec/integration/create_recipe_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'Create a new recipe' do
 
-  it 'should show a recipe list title' do
+  it 'should create a new recipe when all the required fields are specified' do
     visit '/'
     click_link 'New recipe'
     current_path.should == new_recipe_path
@@ -12,6 +12,18 @@ describe 'Create a new recipe' do
     fill_in 'Preparation method', :with => 'secret'
     click_button 'Save'
     Recipe.where(:title => 'Test recipe').count.should == 1
+  end
+
+  it 'should display the form again with an error message when the description is not specified' do
+    visit '/'
+    click_link 'New recipe'
+    current_path.should == new_recipe_path
+    fill_in 'Title', :with => 'Test recipe'
+    fill_in 'Ingredients', :with => 'secret'
+    fill_in 'Preparation method', :with => 'secret'
+    click_button 'Save'
+    Recipe.where(:title => 'Test recipe').count.should == 0
+    page.should have_content "Description can't be blank"
   end
 
 end
