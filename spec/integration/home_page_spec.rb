f840fc7da3ce09496241652374066a154174c2b7
Rieethaa <rieethaa@gmail.com>
2012-02-23 20:10:27 +0000

    Added the menu to the layout. Needs styling

--- spec/integration/home_page_spec.rb
+++ spec/integration/home_page_spec.rb
@@ -11,4 +11,18 @@ describe 'Home page' do
     page.should have_content "Rieethaa's Kitchen"
   end
 
+  it "should have a link to the home page" do
+    visit '/'
+    page.should have_link 'Home'
+    click_link 'Home'
+    current_path.should == root_path
+  end
+
+  it "should have a link to the recipes list" do
+    visit '/'
+    page.should have_link 'Recipes'
+    click_link 'Recipes'
+    current_path.should == recipes_path
+  end
+
 end
