88b1c0fd9bc4a1b8d8037e714abaec9f2e9bd799
Rieethaa <rieethaa@gmail.com>
2012-03-04 14:30:30 +0000

    Resizing the carousel images and some initial styling of the carousel items

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ActiveRecord::Base
   validates_presence_of :title, :description, :ingredients, :preparation_method
-  has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :picture, :styles => { :wide => "960x600#", :large => "800x600>", :medium => "400x300>", :thumb => "120x90>" }
 end
