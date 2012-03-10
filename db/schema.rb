25a17b4251a9c29dab472b6b096b33d604badf76
Rieethaa <rieethaa@gmail.com>
2012-03-10 12:05:24 +0000

    MIgration to create the ingerdient table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120303130240) do
+ActiveRecord::Schema.define(:version => 20120310115454) do
+
+  create_table "ingredients", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "recipes", :force => true do |t|
     t.string   "title"
