1e64050cfb655751a17ea83b4ba26a6f0925673f
Steve Hook <steve.hook@gmail.com>
2012-04-07 13:02:28 +0100

    Layout the recipe list as rows of four side-by-side panels

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ActiveRecord::Base
   validates_presence_of :title, :description, :ingredient_list, :preparation_method
   has_and_belongs_to_many :ingredients
-  has_attached_file :picture, :styles => { :wide => "960x600#", :large => "800x600>", :medium => "400x300>", :thumb => "120x90>" }
+  has_attached_file :picture, :styles => { :wide => "960x600#", :large => "800x600>", :medium => "400x300>", :thumb => "180x180>" }
 
   before_save :save_ingredients
 
