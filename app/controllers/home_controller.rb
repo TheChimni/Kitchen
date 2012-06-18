982ee6556bda371651ccfb5dedda96e1e53614a4
Rieethaa <rieethaa@gmail.com>
2012-06-18 21:36:23 +0100

    Spec for twitter integration. Making sure only 3 tweets are shown on the Home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
     @recipes = Recipe.all
+    @tweets = Twitter.user_timeline('bbcfood').first(3).collect { |tweet| tweet.text }
   end
 end
--- spec/integration/home_page_spec.rb
+++ spec/integration/home_page_spec.rb
@@ -1,9 +1,20 @@
 require 'spec_helper'
 
+
 describe 'Home page' do
   before do
     @recipe = Recipe.create!(:title => 'Aloo tikki chaat', :description => 'Yummy street food',
                              :ingredient_list => 'secret', :preparation_method => 'secret')
+
+    # Stubbing the external twitter API
+    @tweets = ['I fancy chocolate pudding today.', 'I fancy indian veg roll today.', 'I am hungry!', 'I am not feeling well!']
+    Twitter.stub(:user_timeline => @tweets.collect { |tweet| stub(:text => tweet) })
+    # Alternatively, use the below code to stub.
+    # Twitter.stub(:user_timeline => [
+    #   stub(:text => 'I fancy chocolate pudding today.'),
+    #   stub(:text => 'I fancy indian veg roll today.'),
+    #   stub(:text => 'I am hungry!'),
+    #   stub(:text => 'I am not feeling well!')]) 
   end
 
   it 'should show a page header' do
@@ -29,6 +40,18 @@ describe 'Home page' do
     visit '/'
     page.should have_content @recipe.title
     page.should have_content @recipe.description
+  end
+
+  it "should have last 3 tweets" do
+    visit '/'
+    @tweets[0..2].each { |tweet| page.should have_content tweet }
+    page.should_not have_content @tweets[3]
+
+    # Above line of code is a better way of doing the below
+    # ['I fancy chocolate pudding today.', 'I fancy indian veg roll today.', 'I am hungry!'].each do |tweet|
+    #   page.should have_content tweet
+    # end
+    # page.should_not have_content 'I am not feeling well!'
   end
 
 end
