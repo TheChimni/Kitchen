96c5b3e77a5b1803813a394777cf2f2778d42cf7
Rieethaa <rieethaa@gmail.com>
2012-07-19 16:38:05 +0100

    Added some specs for subscription model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,2 +1,3 @@
 class Subscription < ActiveRecord::Base
+  validates_presence_of :email
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,7 +1,62 @@
 require 'spec_helper'
 
 describe Subscription do
-  it "description" do
+
+  it { should validate_presence_of :email }
+
+  it "should allow me to set a name" do
+    subscriber = Subscription.new :name => 'john'
+    subscriber.name.should_not be_nil
+    subscriber.name.should == 'john'
+    subscriber.email.should be_nil
+  end
+
+  it "should set an email address" do
+    subscriber = Subscription.new :email => 'john@gmail.com'
+    subscriber.email.should_not be_nil
+    subscriber.email.should == 'john@gmail.com'
+  end
+
+  it "should set name to nil if only email is provided" do
+    subscriber = Subscription.new :email => 'bob@gmail.com'
+    subscriber.name.should be_nil
+    subscriber.email.should_not be_nil
+    subscriber.email.should == 'bob@gmail.com'
+  end
+
+  it "should set name to nil if only email is provided" do
+    subscriber = Subscription.new :name => 'bob', :email => 'bob@gmail.com'
+    subscriber.name.should_not be_nil
+    subscriber.email.should_not be_nil
+    subscriber.name. should == 'bob'
+    subscriber.email.should == 'bob@gmail.com'
+  end
+
+
+  context "When CREATING subscription" do
+    it "should save the email address of the subscriber in the database" do
+      subscriber = Subscription.create :name => 'bob', :email => 'bob@g.com'
+      subscriber.save
+      Subscription.count.should == 1
+      subscriber.email.should == 'bob@g.com'
+      subscriber.name.should == 'bob'
+    end
+
+    it "should NOT save when no email address is provided" do
+      subscriber = Subscription.create :name => 'bob'
+      subscriber.save
+      Subscription.count.should == 0
+      subscriber.name.should == 'bob'
+    end
+
+    it "should save when no name is provided" do
+      subscriber = Subscription.create :email => 'bob@g.com'
+      subscriber.save
+      subscriber.email.should == 'bob@g.com'
+      subscriber.name.should be_nil
+      Subscription.count.should == 1
+    end
+
 
   end
   
