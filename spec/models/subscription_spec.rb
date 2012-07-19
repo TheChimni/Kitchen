7e5ae238c5013ac0e0cb96c88215578617b31969
Rieethaa <rieethaa@gmail.com>
2012-07-19 20:28:13 +0100

    Corrections to the spec and model

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -3,6 +3,13 @@ require 'spec_helper'
 describe Subscription do
 
   it { should validate_presence_of :email }
+  it { should_not validate_presence_of :name }
+  it { should respond_to :name, :email, :created_at, :updated_at }
+
+  context 'uniqueness constraints' do
+    before { Subscription.create! :email => 'bob@example.com', :name => 'bob' }
+    it { should validate_uniqueness_of :email }
+  end
 
   it "should allow me to set a name" do
     subscriber = Subscription.new :name => 'john'
@@ -34,8 +41,10 @@ describe Subscription do
 
 
   context "When CREATING subscription" do
+
     it "should save the email address of the subscriber in the database" do
       subscriber = Subscription.create :name => 'bob', :email => 'bob@g.com'
+      #  is save required here??
       subscriber.save
       Subscription.count.should == 1
       subscriber.email.should == 'bob@g.com'
@@ -44,6 +53,7 @@ describe Subscription do
 
     it "should NOT save when no email address is provided" do
       subscriber = Subscription.create :name => 'bob'
+      #  is save required here??
       subscriber.save
       Subscription.count.should == 0
       subscriber.name.should == 'bob'
@@ -51,13 +61,23 @@ describe Subscription do
 
     it "should save when no name is provided" do
       subscriber = Subscription.create :email => 'bob@g.com'
+      #  is save required here??
       subscriber.save
       subscriber.email.should == 'bob@g.com'
       subscriber.name.should be_nil
       Subscription.count.should == 1
     end
+  end
 
+  context "business logic validation when CREATING subscription" do
+    before do
+      @subscriber = Subscription.create! :name => 'rob', :email => 'rob@g.com'
+    end
 
+    it "should throw an exception when the same subscriber registers twice" do
+      -> { subscriber = Subscription.create! :name => 'rob', :email => 'rob@g.com' }.should raise_error
+    end
+    
   end
   
 end
