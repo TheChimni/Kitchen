180bf85f29096a4e8c8bfc14affd5003d2869939
Rieethaa <rieethaa@gmail.com>
2012-08-05 13:08:04 +0100

    Control access to new/edit/delete links on the Recipe#show and Recipe#index
    Only display for logged in users.

--- spec/integration/update_recipe_spec.rb
+++ spec/integration/update_recipe_spec.rb
@@ -1,37 +1,47 @@
 require 'spec_helper'
 
 describe 'Update an existing recipe' do
-  include_context :authentication
 
   before do
     @recipe = Recipe.create!(:title => 'Aloo tikki chaat', :description => 'Yummy street food',
                              :ingredient_list => 'secret', :preparation_method => 'secret')
   end
 
-  it 'should update the properties of an existing recipe' do
-    visit recipes_path
-    click_link 'Read >>'
-    current_path.should == recipe_path(@recipe)
-    click_link 'Edit this recipe'
-    current_path.should == edit_recipe_path(@recipe)
-    fill_in 'Description', :with => 'new description'
-    click_button 'Update'
-    @recipe.reload
-    @recipe.description.should == 'new description'
-    current_path.should == recipe_path(@recipe)
+  context 'when not logged in' do
+    it "'Edit this recipe' link should not be present" do
+      visit recipe_path(@recipe)
+      page.should_not have_link 'Edit this recipe'
+    end
   end
 
-  it 'should not update the properties of an existing recipe when I press the Cancel button' do 
-    visit recipes_path
-    click_link 'Read >>'
-    current_path.should == recipe_path(@recipe)
-    click_link 'Edit this recipe'
-    current_path.should == edit_recipe_path(@recipe)
-    fill_in 'Description', :with => 'new description'
-    click_link 'Cancel'
-    @recipe.reload
-    @recipe.description.should == 'Yummy street food'
-    current_path.should == recipe_path(@recipe)
+  context 'when logged in' do
+    include_context :authentication
+
+    it 'should update the properties of an existing recipe' do
+      visit recipes_path
+      click_link 'Read >>'
+      current_path.should == recipe_path(@recipe)
+      click_link 'Edit this recipe'
+      current_path.should == edit_recipe_path(@recipe)
+      fill_in 'Description', :with => 'new description'
+      click_button 'Update'
+      @recipe.reload
+      @recipe.description.should == 'new description'
+      current_path.should == recipe_path(@recipe)
+    end
+
+    it 'should not update the properties of an existing recipe when I press the Cancel button' do 
+      visit recipes_path
+      click_link 'Read >>'
+      current_path.should == recipe_path(@recipe)
+      click_link 'Edit this recipe'
+      current_path.should == edit_recipe_path(@recipe)
+      fill_in 'Description', :with => 'new description'
+      click_link 'Cancel'
+      @recipe.reload
+      @recipe.description.should == 'Yummy street food'
+      current_path.should == recipe_path(@recipe)
+    end
   end
 
 end
