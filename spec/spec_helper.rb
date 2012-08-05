bc61457bcf6264c76dd4d0783adf285f160ad6c0
Rieethaa <rieethaa@gmail.com>
2012-08-05 19:27:00 +0100

    Gets rid of rspec warning.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,5 +46,6 @@ RSpec.configure do |config|
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
 
+  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.include Paperclip::Shoulda::Matchers
 end
