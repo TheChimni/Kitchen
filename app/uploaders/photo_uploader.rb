13ea285cda4cab18905e4c774a2e8591021c60b9
Rieethaa <rieethaa@gmail.com>
2012-09-01 22:07:52 +0100

    Change to carrierwave config to make it work on heroku

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -11,13 +11,16 @@ class PhotoUploader < CarrierWave::Uploader::Base
   include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage :file unless Rails.env.production?
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -37,33 +40,27 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :wide do
-   process :resize_to_fill => [960, 600]
+    process :resize_to_fill => [960, 600]
   end
   version :large do
-   process :resize_to_fill => [800, 600]
+    process :resize_to_fill => [800, 600]
   end
   version :medium do
-   process :resize_to_fill => [500, 375]
+    process :resize_to_fill => [500, 375]
   end
   version :small do
-   process :resize_to_fill => [400, 300]
+    process :resize_to_fill => [400, 300]
   end
   version :thumb do
-   process :resize_to_fill => [280, 280]
+    process :resize_to_fill => [280, 280]
   end
   #has_attached_file :picture, :styles => { :wide => "960x600#", :large => "800x600>", :medium =>"500x375>", :small => "400x300>", :thumb => "280x280>" }
   #has_attached_file :secondary_picture, :styles => { :wide => "960x600#", :large => "800x600>", :medium =>"500x375>", :small => "400x300>", :thumb => "280x280>" }
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
-
-  # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
-  # def filename
-  #   "something.jpg" if original_filename
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
 end
